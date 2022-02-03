d6c032173b5deaf172ffcbb85da6db9ffc0fd7b2
Mei Wu <25568561+projectoriented@users.noreply.github.com>
2022-02-03 12:25:36 +0100

    WIP vcfanno file

--- rucksack/tools/vcfanno.py
+++ rucksack/tools/vcfanno.py
@@ -1,35 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 """Resources required to run VCFAnno"""
-import toml
 
-from pydantic import BaseModel
-from typing import (
-    List, Optional
-)
+import re
+
+from tomlkit import parse
+from typing import List, Optional
+from pydantic import BaseModel, validator
+
+from rucksack.utils.path import copy_files, get_cwd, prepend_path
 
 
-class ConfigToml(BaseModel):
-    block: str = "annotation"
+class Annotation(BaseModel):
+    file: str
     fields: List[str] = ["GQ"]
     ops: List[str] = ["self"]
     names: List[str] = ["exac"]
     columns: Optional[int] = None
     type: Optional[str] = None
 
-
-def generate_toml():
-    pass
-
-
-def generate_lua():
-    pass
-
-
-def serialize(obj_model):
-    serial_item = toml.dumps(obj_model)
-    received_toml = toml.loads(serial_item)
-    return received_toml
+    @validator("file")
+    def name_must_contain_ext(cls, v):
+        ext_regex = re.compile(r'(\.[a-zA-Z]{2,4})$')
+        if not ext_regex.search(v):
+            raise ValueError('must contain a valid extension')
+        return v
 
 
-def write_toml(serialized):
-    with open("data.txt", "w") as writer:
-        writer.write(serialized)
+# def stage_files_to_cwd(base_toml: str, resource_dir: str):
+#     with open(base_toml) as f:
+#         for i, line in enumerate(f):
+#             if line.startswith("file"):
+#                 reference_file = parse(line)["file"]
+#                 absolute_path = prepend_path(reference_file, resource_dir)
+#                 copy_files(absolute_path, get_cwd())
